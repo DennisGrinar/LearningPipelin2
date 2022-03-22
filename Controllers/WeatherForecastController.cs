c0ddf80a9d7be339856a88c63714bcc50f6514d1
Dennis Grinar <dennis.grinar.jr@gmail.com>
2022-03-22 15:51:32 -0400

    edit weather summaries

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,8 @@ namespace LearningPipelin2.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "FRICKIN COLD"
+            //"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
